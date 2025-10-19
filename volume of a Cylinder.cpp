a10b830a251746b629c45c9e988198127f476d20
sharlyn77 <sharlyn.msd@gmail.com>
2025-10-19 20:29:21 +0600

    Add files via upload

---
+++ volume of a Cylinder.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+ int r,h;
+ float vol;
+
+ cout <<"input the radius of the cylinder:";
+ cin >> r ;
+
+ cout <<"input the height of the cylinder:";
+ cin >> h ;
+
+ vol = 3.14 * r * r * h;
+
+ cout <<"The volume of a cylinder:"<< vol <<endl;
+ cin >> vol ;
+
+}
